f1a8bc0f5be90437ee68fb3f06f37bea46243df0
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2015-04-27 12:49:36 +0200

    Remade the interface

--- tools/calibrator/src/MainWindow.cpp
+++ tools/calibrator/src/MainWindow.cpp
@@ -5,8 +5,19 @@ MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 	, d_ui(new Ui::MainWindow) {
     d_ui->setupUi(this);
+    
+    d_ui->toolButton->setDefaultAction(d_ui->actionSave_in_EEPROM);
+
+
+
+    
 }
 
 MainWindow::~MainWindow() {
     delete d_ui;
+}
+
+
+void MainWindow::on_actionQuit_triggered() {
+	this->close();
 }
--- tools/calibrator/src/MainWindow.h
+++ tools/calibrator/src/MainWindow.h
@@ -14,6 +14,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected slots:
+	void on_actionQuit_triggered();
+
 private:
     Ui::MainWindow * d_ui;
 };
