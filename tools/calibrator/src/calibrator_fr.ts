6de5beb597cdfc96e31f80782beb308a2cecdcc1
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2015-04-29 10:37:59 +0200

    Finishes french translation draft

--- tools/calibrator/src/calibrator_fr.ts
+++ tools/calibrator/src/calibrator_fr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<TS version="2.0" language="fr_FR">
 <context>
     <name>MainWindow</name>
     <message>
@@ -73,6 +73,71 @@
         <location filename="MainWindow.cpp" line="77"/>
         <source>List of device refreshed</source>
         <translation>Liste des appareils connectés mise à jour</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>C</source>
+        <translation>Do</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>C#</source>
+        <translation>Do#</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>D</source>
+        <translation>Ré</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>D#</source>
+        <translation>Ré#</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>E</source>
+        <translation>Mi</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>F</source>
+        <translation>Fa</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>F#</source>
+        <translation>Fa#</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>G</source>
+        <translation>Sol</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>G#</source>
+        <translation>Sol#</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>A</source>
+        <translation>La</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>B</source>
+        <translation>Si</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="111"/>
+        <source>A#</source>
+        <translation>La#</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="116"/>
+        <source>%1 %2</source>
+        <translation>%1 %2</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="121"/>
