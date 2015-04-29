869e1d12ccc40e24e3085d422bb6d5c86a528003
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2015-04-29 10:11:23 +0200

    Adds french translation

---
+++ tools/calibrator/src/calibrator_fr.ts
@@ -0,0 +1,141 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.ui" line="26"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="57"/>
+        <source>Device Management</source>
+        <translation>Gestion de l&apos;appareil</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="75"/>
+        <location filename="MainWindow.ui" line="225"/>
+        <source>Save in EEPROM</source>
+        <translation>Sauver dans l&apos;EEPROM</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="100"/>
+        <source>QGroupBox</source>
+        <translation>QGroupBox</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="106"/>
+        <source>Minimum Value</source>
+        <translation>Valeur Minimale</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="120"/>
+        <source>Maximum Value</source>
+        <translation>Valeur Maximale</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="194"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="206"/>
+        <source>Refresh Devices</source>
+        <translation>Rafraichir la liste des appareils</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="209"/>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="214"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="217"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="228"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="66"/>
+        <source>Could not save data in EEPROM, device Closed</source>
+        <translation>L&apos;écriture dans l&apos;EEPROM à échoué, connection à l&apos;appareil interrompue</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="77"/>
+        <source>List of device refreshed</source>
+        <translation>Liste des appareils connectés mise à jour</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="121"/>
+        <source>Note</source>
+        <translation>Note</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="121"/>
+        <source>Value</source>
+        <translation>Valeur</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="125"/>
+        <source>Opened device at %1:%2</source>
+        <translation>Connecté à l&apos;appareil présent en %1:%2</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="141"/>
+        <source>Closed device at %1:%2</source>
+        <translation>Deconnexion de l&apos;appareil en %1:%2</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="190"/>
+        <source>Cell: %1</source>
+        <translation>Cellule: %1</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="191"/>
+        <source>Selected Cell for %1</source>
+        <translation>Cellule de la note %1 selectionée</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="203"/>
+        <source>Cell: None</source>
+        <translation>Cellule: aucune</translation>
+    </message>
+</context>
+<context>
+    <name>Oscilloscope</name>
+    <message>
+        <location filename="Oscilloscope.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Oscilloscope.ui" line="20"/>
+        <source>Trigger:</source>
+        <translation>Trigger:</translation>
+    </message>
+    <message>
+        <location filename="Oscilloscope.ui" line="33"/>
+        <source>Time Scale:</source>
+        <translation>Echelle de Temps:</translation>
+    </message>
+    <message>
+        <location filename="Oscilloscope.ui" line="50"/>
+        <source>Value Max:</source>
+        <translation>Valeur Minimale:</translation>
+    </message>
+    <message>
+        <location filename="Oscilloscope.ui" line="73"/>
+        <source>Value Min:</source>
+        <translation>Valeur Maximale:</translation>
+    </message>
+</context>
+</TS>
